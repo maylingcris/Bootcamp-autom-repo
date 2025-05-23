751e8676188114fd73f3352e9272571a5e14f8b7
marboleda <maylingcristina.arboleda@rockwellautomation.com>
2025-05-23 16:04:33 -0500

    changes

--- Capitalized.ts
+++ Capitalized.ts
@@ -3,3 +3,5 @@ const words: string[] = ["hello", "world", "typescript"];
 const capitalizedWords: string[] = words.map(str => str.charAt(0).toUpperCase() + str.slice(1));
 
 console.log(capitalizedWords);
+
+//new change
